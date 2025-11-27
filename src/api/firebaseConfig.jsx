fe80fb4eaca87590a38d0e033930a53638365a9e
CHETAN1675 <mahajanchetan001@gmail.com>
2025-11-27 21:46:23 +0530

    Setup of  AuthContext and Firebase Auth REST structure

---
+++ src/api/firebaseConfig.jsx
@@ -0,0 +1,2 @@
+export const FIREBASE_API_KEY =  "AIzaSyCAX2d8hHqA5T6nWDdGYiv5ZDLSSSi9_-o";
+export const FIREBASE_AUTH_BASE_URL = "https://identitytoolkit.googleapis.com/v1";
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import store from './Store/store.jsx';
+import { AuthProvider } from './context/AuthContext.jsx';
 
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
  <StrictMode>
-    <App />
+  <AuthProvider>
+ <App />
+  </AuthProvider>
   </StrictMode>
   </Provider>
  ,
